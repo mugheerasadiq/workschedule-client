d442240b7f9d5db280338c337e859d76fbf0a4be
rldnd <gi43890@naver.com>
2021-01-22 17:52:23 +0900

    add isoWeek 로직, work container tag table - #75

--- src/components/TagTable/TagTable.js
+++ src/components/TagTable/TagTable.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import * as styled from './styled';
 
-export default function TagTable() {
+const columns = [
+	{
+		key: 'name',
+		dataIndex: 'name',
+		title: '조 이름',
+		align: 'center',
+	},
+	{
+		key: 'start',
+		dataIndex: 'start',
+		title: '시작 시간',
+		align: 'center',
+	},
+	{
+		key: 'end',
+		dataIndex: 'end',
+		title: '끝 시간',
+		align: 'center',
+	},
+	{
+		key: 'time',
+		dataIndex: 'time',
+		title: '근무 시간',
+		align: 'center',
+	},
+];
+
+export default function TagTable({ tagList = [] }) {
 	return (
 		<styled.TagTableWrapper>
-			<styled.TagTable scroll={{ y: 300 }} />
+			<styled.TagTable
+				rowKey="name"
+				columns={columns}
+				dataSource={tagList}
+				pagination={false}
+				scroll={{ y: 300 }}
+			/>
 		</styled.TagTableWrapper>
 	);
 }
